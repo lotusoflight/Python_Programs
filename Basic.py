ba991d68709a763ed35c874f627215d1de20c719
Gopika m <gopikam0405@gmail.com>
2026-08-03 18:45:39 +0530

    py

--- Basic.py
+++ Basic.py
@@ -1,1 +1,169 @@
-print("hello world")
+# print("hello world")
+
+
+#(Delimiters-in-python)
+
+# a="anu devi priya"
+# a="anu-devi priya"
+# b=a.split("-")
+# print(b));
+
+# (Escape-in-python)
+
+# a="this is python basic class\n\t my fav sub is python"
+# print(a)
+
+# (Variable-in-python)
+
+# a="arun"
+# b=23
+# c=4.5
+# d=True
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# (Data Type-in-python)
+
+# print("Name is:",a)
+# print("age is:",b)
+# print("grade is:",c)
+# print("Allowed is:",d)
+
+# print(a,b,c,d)
+# print("name ",a," age ",b," grade ",c," allowed ",d)
+
+
+# formated print
+# print(f"name is {a} age {b} grade {c} allowed {d}")
+
+
+
+# name=input("Enter value:")
+# print(name)
+# print(type(name))
+
+
+# name=int(input("Enter value:"))
+# name=str(input("Enter value:"))
+# name=float(input("Enter value:"))
+# name=bool(input("Enter value:")));
+
+
+
+
+# operators
+
+# arithmetic--- +,-,*,/,//,%
+
+# a=5
+# b=4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+
+
+# assignment --  +=,-=,*=,/=,//=,%=
+# a+=b--->a=a+b
+
+
+# a=5
+# b=3
+# a+=b
+# print(a)
+# print(a)
+# a-=b
+# print(a)
+# a*=b
+# print(a)
+# a/=b
+# print(a)
+# a//=b
+# print(a)
+# a%=b
+# print(a)
+
+
+# Relational (Comparison)
+# >,<,>=,<=!=,==
+# output-->true/false
+
+# a=5
+# b=5   #or b=4
+# print(a>b)
+# print(a>=b)
+# print(a<b)
+# print(a<=b)
+# print(a!=b)
+# print(a==b)
+
+
+
+# Logical oper
+# and,or,not
+
+# a=5
+# b=3
+# print(a>b and a<b)
+# print(a>b or a<b)
+# print(not(a>b and a<b))
+
+# bitwise binary(2)
+# &,|,^,~,>>,<<
+# a=20
+# b=18
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(~a)
+
+
+# a=20
+# b=a>>2
+# c=a<<2
+# print(b)
+# print(c)
+
+
+# Identity
+# a="python"
+# b="Python"
+# c="python"
+# print(a is b)
+# print(a is not b)
+# print(a is c)
+# print(a is not c)
+
+
+
+# Membership
+# x="n"
+# y="m"
+# a="python"
+# print(x in a)
+# print(x not in a)
+# print(y in a)
+# print(y not in a)
+
+# x="lang"
+# y="money"
+# a="python is a program lang"
+# print(x in a)
+# print(x not in a)
+# print(y in a)
+# print(y not in a)
+
+
+# Ternary
+# not allowed
+# ? :  X
+# c=5>3?"true":"fLSE"
+
+# c= 5 if 5>10 else 10
+# print("res:",c)
