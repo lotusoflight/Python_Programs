d28d1a963345999450387e05198aeb7f12490ec3
Gopika m <gopikam0405@gmail.com>
2026-08-13 11:34:05 +0530

    py

---
+++ Setpro.py
@@ -0,0 +1,90 @@
+# a={2}
+# print(a,type(a))
+
+
+# b=set()
+# print(b,type(b))
+
+# a=set()
+# a.add(10)
+# a.add(70)
+# a.add(3)
+# a.add(6)
+# print(a)
+
+
+
+
+# b=a.copy()
+# print(b)
+
+
+# a.discard(70)
+# print(a)
+
+
+# a.remove(13)
+# print(a)
+
+# a.pop()
+# print(a)
+
+# b={20,50,40,60,3}
+# a.update(b)
+# a.update({20,20,60,3,3,3,3})
+# print(a)
+
+
+# a.clear()
+# print(a)
+
+
+
+# methmatical set methods
+
+# a={10,20,30,40,50}
+# b={20,30,60,70,80}
+# print(a)
+# print(a.difference(b))
+# print(a)
+# a.difference_update(b)
+# print(a)
+
+
+# print(a.symmetric_difference(b))
+# print(a)
+# a.symmetric_difference_update(b)
+# print(a)
+
+
+# print(a.intersection(b))
+# print(a)
+# a.intersection_update(b)
+# print(a)
+
+
+# print(a.union(b))
+
+
+
+
+# a={1,2,3}
+# b={2,3,1}
+# # print(a.isdisjoint(b))
+# # print(a.issubset(b))
+# print(a.issuperset(b))
+
+# a={1,2,3}
+# b={4,5,6}
+# # print(a.isdisjoint(b))
+# # print(a.issubset(b))
+# print(a.issuperset(b))
+
+
+
+# a={7,8,9}
+# b={9,4,2}
+# # print(a.isdisjoint(b))
+# # print(a.issubset(b))
+# print(a.issuperset(b))
+
