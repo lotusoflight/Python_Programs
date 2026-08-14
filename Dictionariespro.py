7fb98d27374155948807986e38f582515e714a1e
Gopika m <gopikam0405@gmail.com>
2026-08-14 11:48:38 +0530

    py

---
+++ Dictionariespro.py
@@ -0,0 +1,39 @@
+# a={}
+# b=dict()
+
+# a={"name":"anu","age":23}
+# print(a)
+# print(a["name"])
+# print(a.get("age"))
+
+# b=a.copy()
+# print(a)
+# a.clear()
+# print(a)
+
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+
+# a["city"]="kochi"
+# print(a)
+# a.setdefault("mark",100)
+# print(a)
+
+# a["name"]="priya"
+# print(a)
+# a.update({"age": 15})
+# print(a)
+
+
+# a.pop("age")
+# print(a)
+
+# a.popitem()
+# print(a)
+
+
+
+# a=["anu","priya","devi"]
+# b=dict.fromkeys(a,89)
+# print(b)
