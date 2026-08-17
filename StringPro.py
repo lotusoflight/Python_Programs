d0a952e4259f7d074ac8adf28bf1f7ae848afd02
Gopika m <gopikam0405@gmail.com>
2026-08-17 11:27:49 +0530

    py

---
+++ StringPro.py
@@ -0,0 +1,101 @@
+# a="python"
+# print(a)
+# print(a+"programs")
+# print(a)
+
+
+# a='dgsdg'
+# b="cfvgbhnjmk"
+# c='''dcfvgbhnjmk,
+# vgbhnjmk,l.
+# dfg'''
+# d="""
+# fghjkl
+# cvbnm
+# dfrg
+# sdfg
+# """
+
+
+# a="python"
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+# print(a[-4])
+# print(a[-5])
+# print(a[-6])
+
+
+# a="python"
+# b="ANT"
+# c="gSbAEF"
+# d="python programms"
+# e="python is a program"
+# print(a.upper())
+# print(b.lower())
+# print(c.swapcase())
+# print(d.capitalize())
+# print(e.title())
+
+
+
+# a="python"
+# b="python is a program lang python"
+# print(a.find('z'))
+# print(b.find('lang'))
+
+# print(b.index("zoom"))
+# print(b.index("python",15))
+
+# print(b.count("python"))
+
+# print(a.replace('y','i'))
+# print(b.replace('python','java'))
+
+
+
+
+# b="python is a-program lang-python"
+
+# print(b.split(" "))
+# print(b.split("-"))
+
+
+# a=["anu","priya","devi","kavi"]
+# b=" ".join(a)
+# print(b)
+
+
+# a=" python "
+# print(a.lstrip())
+# print(a.rstrip())
+# print(a.strip())
+
+
+# a="Python is a program lang"
+# print(a.startswith("Python"))
+# print(a.startswith("java"))
+
+# print(a.endswith("lang"))
+# print(a.endswith("python"))
+
+
+
+# a="ertyui"
+# b="34567890"
+# c="dfgh567"
+# d=" "
+# print(a.isalpha())
+# print(b.isnumeric())
+# print(c.isalnum())
+# print(d.isspace())
+
+
+
